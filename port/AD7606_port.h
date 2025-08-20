1be633b16c97be006f95b4670b4bfddbda203568
Hossein.M <hossein.m9877@gmail.com>
2025-08-20 14:46:00 +0330

    feat: add AD7606_port.h

--- port/AD7606_port.h
+++ port/AD7606_port.h
@@ -1,13 +1,13 @@
 /**
   ******************************************************************************
-  * File Name          : AD7606.h
-  * Description        : AD7606 ADC Driver header file.
-	* Last Update				 : 26-Dec-2020
-	*	Author						 : Mohammad Sayadi
-	* E-Mail						 : mo.sayadi@gmail.com
+  * File Name          : AD7606_port.h
+  * Description        : AD7606 ADC Driver port header file.
+	* Last Update				 : 20-Aug-2025
+	*	Author						 : Hossein.M
+	* Github						 : https://github.com/Hossein-M98
   ******************************************************************************
   *
-  * Copyright (c) 2020 Mahda Embedded Systems 
+  * Copyright (c) 2025 Mahda Embedded Systems 
   * All rights reserved.
   *
   * Redistribution and use in source and binary forms, with or without 
@@ -44,35 +44,41 @@
   ******************************************************************************
   */
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __AD7606_H
-#define __AD7606_H
+#ifndef __AD7606_PORT_H
+#define __AD7606_PORT_H
 
 /* Includes ------------------------------------------------------------------*/
-#include <stdint.h>
-#include <stdbool.h>
-/* Private define ------------------------------------------------------------*/
-enum Enum_AD7606_OverSampling {
-	AD7606_OverSampling_NoOS = 0x00,
-  AD7606_OverSampling_2 = 0x01,
-  AD7606_OverSampling_4 = 0x02,
-  AD7606_OverSampling_8 = 0x03,
-  AD7606_OverSampling_16 = 0x04,
-  AD7606_OverSampling_32 = 0x05,
-  AD7606_OverSampling_64 = 0x06,
-  AD7606_OverSampling_Invalid = 0x07,
-};
-typedef enum Enum_AD7606_OverSampling Type_AD7606_OverSampling;
+#include "stm32f7xx_hal.h"
+#include "main.h"
 
-enum Enum_AD7606_Range {
-	AD7606_Range_5V = 0x00,
-  AD7606_Range_10 = 0x01,
-};
-typedef enum Enum_AD7606_Range Type_AD7606_Range;
+#define AD7606_MS0_GPIO     AD7606_MS0_GPIO_Port
 
-/* Functions -----------------------------------------------------------------*/
-void AD7606_Init(uint8_t cardCount, bool OS1, bool OS2, bool range);
-void AD7606_Read_OneCard(uint8_t cardNo, int16_t * data);
-void AD7606_Read_All(int16_t * data);
+#define AD7606_MCS_GPIO     AD7606_MCS_GPIO_Port
+#define AD7606_MCS_PIN      AD7606_MCS_Pin
 
-#endif /* __AD7606_H */
+#define AD7606_RDSCLK_GPIO  AD7606_RDSCLK_GPIO_Port
+#define AD7606_RDSCLK_PIN   AD7606_RDSCLK_Pin
+
+#define AD7606_CONVST_GPIO  AD7606_CONVST_GPIO_Port
+#define AD7606_CONVST_PIN   AD7606_CONVST_Pin
+
+#define AD7606_RESET_GPIO   AD7606_RESET_GPIO_Port
+#define AD7606_RESET_PIN    AD7606_RESET_Pin
+
+#define AD7606_RANGE_GPIO   AD7606_RANGE_GPIO_Port
+#define AD7606_RANGE_PIN    AD7606_RANGE_Pin
+
+#define AD7606_OS1_GPIO     AD7606_OS1_GPIO_Port
+#define AD7606_OS1_PIN      AD7606_OS1_Pin
+
+#define AD7606_OS2_GPIO     AD7606_OS2_GPIO_Port
+#define AD7606_OS2_PIN      AD7606_OS2_Pin
+
+#define AD7606_BUSY_GPIO    AD7606_BUSY_GPIO_Port
+#define AD7606_BUSY_PIN     AD7606_BUSY_Pin
+
+#define AD7606_D0_GPIO      AD7606_D0_GPIO_Port
+#define AD7606_D8_GPIO      AD7606_D8_GPIO_Port
+
+#endif /* __AD7606_PORT_H */
 /************************ (C) COPYRIGHT MAHDA EMBEDDED SYSTEMS *****END OF FILE****/
